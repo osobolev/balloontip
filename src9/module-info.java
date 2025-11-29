8b400b2fa37f8e30bb39f9c4de429368a68523cb
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-29 13:07:49 +0100

    Simplify Java 9 module info compilation

--- src9/module-info.java
+++ src9/module-info.java
@@ -4,5 +4,7 @@ module io.github.osobolev.balloontip {
     exports net.java.balloontip.styles;
     exports net.java.balloontip.utils;
 
+    opens net.java.balloontip.images;
+
     requires transitive java.desktop;
 }
